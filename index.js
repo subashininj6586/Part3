cb5928bb816ee377a1f7d9f4a50b9b2fbfb05d6e
subashininj6586 <subashininj@kluniversity.in>
2021-05-21 15:30:10 +0530

    3.1: Phonebook backend step1. Phone book info displayed

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  
+]
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(persons))
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
