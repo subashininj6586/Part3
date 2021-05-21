73f9e5b7da7fb34fb95f509bc6dc97c8d090455e
subashininj6586 <subashininj@kluniversity.in>
2021-05-21 19:20:36 +0530

    3.5: Phonebook backend step5 adding person

--- index.js
+++ index.js
@@ -1,48 +1,63 @@
+const express = require('express');
 
-const express = require('express')
-const app = express()
+const app = express();
+app.use(express.json())
 
 let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "subashini",
-    number: "1223445",
-    id: 4
-  },
-]
+      {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+      },
+      {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
+      },
+      {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+      },
+      {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
+      }
+    ]
+    const generateId = () => {
+      const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+      return maxId + 1
+    }
+  
+    app.post('/api/persons', (request, response) => {
+      const body = request.body
+      
 
+      if (!body.name) {
+        return response.status(400).json({ 
+          error: 'content missing' 
+        })
+      }
+   
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  console.log(id)
-  const person = persons.find(person => person.id === id)
-  console.log( person)
-  if ( person) {
-    response.json( person)
-  } else {
-    response.status(404).end()
-  }
-})
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+      const person = {
+        name: body.name,
+        number:body.number,
+        id: generateId(),
+      }
+      persons = persons.concat(person)
+  
+    response.json(person)
+     }) 
+ 
+  
+app.get('/api/persons',(req,res) => {
+    res.json(persons)
+});
 
-  response.status(204).end()
-})
 
 const PORT = 3001
 app.listen(PORT, () => {
